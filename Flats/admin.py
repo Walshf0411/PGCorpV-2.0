019998625fe66b3561e6f640bd9179142c63cb18
Walsh Fernandes <2016.walsh.fernandes@ves.ac.in>
2018-09-26 14:34:17 +0530

    Flat post view done

--- Flats/admin.py
+++ Flats/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Flat, FlatDetails
 # Register your models here.
+
+admin.site.register(Flat)
+admin.site.register(FlatDetails)
--- Flats/urls.py
+++ Flats/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from .import views
 
 urlpatterns = [
-	url(r'post/$', views.FlatPostView.as_view(), name='post_flat'),
+	url(r'post/$', views.FlatPostView.as_view(), name='post'),
 ]
