b50c7d4bd4e5fc0ff388913cd4a295aec90b8ac5
Walsh Fernandes <2016.walsh.fernandes@ves.ac.in>
2019-02-04 22:54:56 +0530

    chagned listview and listview connected with detail view.

--- Flats/urls.py
+++ Flats/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
 	url(r'post/$', views.FlatPostView.as_view(), name='post'),
 	url(r'(?P<hash>[\w-]{6})/(?P<slug>[\w-]+)/$', views.FlatDetailsView.as_view(), name='flat_detail'),
 	url(r'all/$', views.FlatsListView.as_view(), name='all_flats'),
-	url(r'(?P<hash>[\w-]{6})/(?P<slug>[\w-]+)/contac_owner/$', views.ContactOwnerView.as_view(), name='contact_owner'),
+	url(r'(?P<hash>[\w-]{6})/(?P<slug>[\w-]+)/contact_sowner/$', views.ContactOwnerView.as_view(), name='contact_owner'),
 ]
