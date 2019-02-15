c1a31e9574d5bb4066c2059a8ffba5f722c1c9bd
mohit-makhija <2016.mohit.makhija@ves.ac.in>
2019-02-15 10:27:15 +0530

    Flat model added, along with basic form for posting flat.

--- Flats/forms.py
+++ Flats/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from .models import FlatDetails
 
+
 class FlatPostForm(forms.ModelForm):
-    rent =  forms.IntegerField(widget=forms.NumberInput(attrs={"value": 200}))
+    rent = forms.IntegerField(widget=forms.NumberInput(attrs={"value": 200}))
     
     class Meta:
-        fields = ['title', 'location', 'rent']
+        fields = ['title', 'address', 'description', 'total_rent', 'total_space', 'total_rent', 'deposit', 'possession', 'total_rooms', 'property_type', 'floor', 'parking', 'number_of_guests']
         model = FlatDetails
--- Flats/models.py
+++ Flats/models.py
@@ -5,14 +5,25 @@ from django.utils.text import slugify
 from django.core.validators import MinValueValidator
 # Create your models here.
 
+
 class FlatDetails(models.Model):
 	'''This is a seperate table as the details can be comfortably increased in future'''
 	# The id of the flat for which we have to store the details
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	location = models.CharField(max_length=30)
-	title = models.CharField(max_length=30, default="default")
+	title = models.CharField(max_length=30)
+	address = models.CharField(max_length=30)
+	description = models.CharField(max_length=200)
+	total_space = models.CharField(max_length=10)
+	total_rent = models.IntegerField(validators=(MinValueValidator(limit_value=200), ))
+	deposit = models.CharField(max_length=10)
+	possession = models.CharField(max_length=20)
+	datetime = models.DateField(auto_now=True)
+	total_rooms = models.IntegerField(validators=(MinValueValidator(limit_value=1), ))
+	property_type = models.CharField(max_length=30)
+	floor = models.CharField(max_length=4)
+	parking = models.BooleanField(default=False)
+	number_of_guests = models.IntegerField(validators=(MinValueValidator(limit_value=1), ))
 	hash = models.CharField(max_length=6)
-	rent =  models.IntegerField(validators=(MinValueValidator(limit_value=200), ))
 	slug = models.SlugField(null=True)
 
 	def save(self, *args, **kwargs):
